336e07d8f57e80d954ff1715434204bcd1808ad1
Jean Pierre <frankjeanpierregs@ufps.edu.co>
2021-02-23 13:36:37 -0500

    agregando ingredientes a productos

--- src/controllers/IngredienteProductoController.js
+++ src/controllers/IngredienteProductoController.js
@@ -1,11 +1,13 @@
 const { IngredienteProducto } = require("../repository/database/index").models;
 
 module.exports = {
-    create: async (id_producto, ingredientes) => {
-        const ingredienteProducto = ingredientes.map((item) => {
-            return { id_producto, id_ingrediente: item, extra: false };
-        });
-        await IngredienteProducto.bulkCreate(ingredienteProductoDB);
-        return ingredienteProducto ? true : false;
-    },
+	create: async (id_producto, ingredientes) => {
+		const ingredienteProducto = ingredientes.map((item) => {
+			return { id_producto, id_ingrediente: item, extra: false };
+		});
+		const rowsDB = await IngredienteProducto.bulkCreate(ingredienteProducto, {
+			returning: true,
+		});
+		return rowsDB ? true : false;
+	},
 };
